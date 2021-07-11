d56624780017f728d7ecf82d5ab37a7ce88d8328
Patrick Moores <patrickmoores@Patricks-MacBook-Air.local>
2021-07-11 17:01:54 -0400

    Adding PyPoll.py file try again.

--- Pypoll.py
+++ Pypoll.py
@@ -8,14 +8,51 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Open the file using a with statement
+
+# The data we need to retrieve
+# 1. Initialize a vote counter to zero
+total_votes = 0
+
+# Initialize Candidate Options variable
+candidate_options = []
+
+# Declare empty candidate votes dictionary
+candidate_votes = {}
+
+# Open the election results using a with statement
 with open(file_to_load) as election_data:
     # Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
-    # Read a print the header row
+    # Read the header row
     headers = next(file_reader)
-    print(headers)
+    
+    # Print each row in the csv file
+    for row in file_reader:
+        # 2. Add to the total votes count
+        total_votes += 1
+        
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            # add it to the list of candidates
+            candidate_options.append(candidate_name)
+
+            # 2. Begin tracking that candidate's vote count:
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+
+# 3 Print the candidate vote dictionary
+print(candidate_votes)
+
+
+
+
+
 
 
 # Open the election results and read the file (using 'with statement')
